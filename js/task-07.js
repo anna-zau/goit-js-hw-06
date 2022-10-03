e4b3af6077dad3a4c646f07ea3cfd91baed78769
anna-zau <annazauholnikova555@gmail.com>
2022-10-03 18:00:39 +0300

    Update 7 task

--- js/task-07.js
+++ js/task-07.js
@@ -2,5 +2,5 @@ const inputSlider = document.querySelector("#font-size-control");
 const textOutput = document.querySelector("#text");
 
 inputSlider.addEventListener("input", (event) => {
-  text.style.fontSize = inputSlider.value + "px";
+  textOutput.style.fontSize = inputSlider.value + "px";
 });
